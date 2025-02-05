d1b77ab905ff9714b342d2842046902dc2190e32
dimsth <dimo35xf@gmail.com>
2025-02-05 18:09:40 +0200

    [refactor] Add HOST as an environment variable and rename index to app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const temporalRoutes = require("./routes/temporalEntities");
+const temporalRoutes = require("./routes/temporalRoute");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "http://localhost";
 
 const app = express();
 app.use(morgan("dev"));
@@ -14,5 +15,5 @@ app.use("/ngsi-ld/v1/temporal/entities", temporalRoutes);
 
 // Start the Express server
 app.listen(PORT, () => {
-    console.log(`HTTP Bridge is running on http://localhost:${PORT}`);
+    console.log(`HTTP Bridge is running on ${HOST}:${PORT}`);
 });
