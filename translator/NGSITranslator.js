150b9914e230e003c2b3a888bdfb8b7c54eab1de
dimsth <dimo35xf@gmail.com>
2025-02-05 19:53:06 +0200

    [fix] Translator missing context

--- translator/NGSITranslator.js
+++ translator/NGSITranslator.js
@@ -6,17 +6,21 @@ class NGSITranslator {
         if (!typeMap) {
             throw new Error(`No mapping found for entity type: ${entityType}`);
         }
-
+    
         const groupedByDevice = apiData.reduce((acc, item) => {
             const { device_id, modality_id, value, observed_at } = item;
-
+    
             if (!acc[device_id]) {
                 acc[device_id] = {
                     id: `urn:ngsi-ld:Device:${device_id}`,
                     type: entityType,
+                    "@context": [
+                        "https://raw.githubusercontent.com/smart-data-models/dataModel.Weather/master/context.jsonld",
+                        "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"
+                    ]
                 };
             }
-
+    
             const modalityInfo = typeMap[modality_id];
             if (modalityInfo) {
                 const property = NGSITranslator.createProperty(
@@ -26,33 +30,38 @@ class NGSITranslator {
                     modalityInfo.max,
                     modalityInfo.slug
                 );
-
+    
                 if (property !== undefined) {
                     acc[device_id][modalityInfo.key] = property;
                 }
             }
-
+    
             if (observed_at) {
                 acc[device_id]["dateObserved"] = { type: "Property", value: observed_at };
             }
-
+    
             return acc;
         }, {});
-
+    
         return Object.values(groupedByDevice);
-    }
+    }    
 
-    static createProperty(value, unit, min, max, slug) {
+    static createProperty(value, unit, min, max, slug, observedAt = null) {
         if (value == null) return undefined;
-
+    
         return {
             type: "Property",
-            value: value,
-            metadata: {
-                unitCode: { type: "Property", value: unit },
-                healthRange: { type: "Property", value: { min, max } },
-                slug: { type: "Property", value: slug },
-            },
+            values: [
+                {
+                    value: value,
+                    metadata: {
+                        unitCode: { type: "Property", value: unit },
+                        healthRange: { type: "Property", value: { min, max } },
+                        slug: { type: "Property", value: slug }
+                    },
+                    ...(observedAt ? { observedAt: observedAt } : {}) // Include `observedAt` only if present
+                }
+            ]
         };
     }
 }
